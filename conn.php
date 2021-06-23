8452633c4bbde8599158d6ae94e0e600bebbe0bb
Abbas Kagdi <24469915+AbbasKagdi@users.noreply.github.com>
2021-06-23 14:29:55 +0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -5,8 +5,8 @@ use Automattic\WooCommerce\Client;
 
 $woocommerce = new Client(
     'https://tpgreen.shop/',
-    'ck_f051cb51f8270a0f23fea0e880a3f94252809078',
-    'cs_4b3c7e049485f456d612a92c9e3b4487a527be87',
+    'ck_xxx',
+    'cs_xxx',
     [
         'wp_api' => true,
         'version' => 'wc/v3',
